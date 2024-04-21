7e993085ddb2f2a518b57763fa0461ffef6f5867
Laurence Unabia <laurenceunabia@gmail.com>
2024-04-21 14:21:27 +0800

    Code changes in Player tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <p>Coming soon...</p>
-    
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="O"/>          
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
